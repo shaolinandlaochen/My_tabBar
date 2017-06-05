7f233d3419caff596545df6df36fc5b899474adb
123 <a123@123deMac-mini.local>
2017-06-05 13:58:18 +0800

    Initial Commit

---
+++ 自定义tabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自定义tabBar
+//
+//  Created by 123 on 2017/6/5.
+//  Copyright © 2017年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 自定义tabBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  自定义tabBar
+//
+//  Created by 123 on 2017/6/5.
+//  Copyright © 2017年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
